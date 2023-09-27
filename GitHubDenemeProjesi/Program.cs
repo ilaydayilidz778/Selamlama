88d70004999dd22e235b838b55fbbb3f5bc1129b
İlayda Yıldız <146078544+ilaydayilidz778@users.noreply.github.com>
2023-09-27 14:21:07 +0300

    Add project files.

---
+++ GitHubDenemeProjesi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitHubDenemeProjesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba İlayda");
+        }
+    }
+}
